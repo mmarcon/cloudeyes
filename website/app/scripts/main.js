da51579f2967e32dd810a38f5df6ab9170f0e5b4
Massimiliano Marcon <me@marcon.me>
2013-07-27 23:07:03 +0200

    Topbar

--- website/app/scripts/main.js
+++ website/app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+    var body = $('body'),
+        container = $('.container');
+    $(window).on('scroll', function(){
+        if(body.scrollTop() >= 400) {
+            return container.addClass('sticky-header');
+        }
+        container.removeClass('sticky-header');
+    });
+});
