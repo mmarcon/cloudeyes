cd44376b7531f26f5ce3e40c608c554ece93478f
Massimiliano Marcon <emaildelmax@gmail.com>
2013-07-30 08:32:05 -0400

    HTTP Analyzer.
    Retrieves HTTP status code and response body given a URL.
    Includes functional tests. Unit tests on the way.

---
+++ tests/functional/http-analyzer.func.js
@@ -0,0 +1,32 @@
+var Actions = require('../../lib/actions'),
+    assert = require('assert'),
+    clc = require('cli-color'),
+    Analyzer = Actions.Analyzer,
+    HTTPAnalyzer = Actions.HTTPAnalyzer;
+    
+var BASE_URL = 'http://mock.isssues.com/';
+
+var tests = [
+    {code: 200, message: 'Successful'},
+    {code: 400, message: 'Bad Request'},
+    {code: 404, message: 'Not Found'},
+    {code: 500, message: 'Internal Server Error'}
+];
+
+var testFn = function(test) {
+    var targetObject = {
+        url: BASE_URL + test.code
+    };
+    Analyzer.with(targetObject).then(HTTPAnalyzer.analyze).then(function(o){
+        try {
+            assert.equal(BASE_URL + test.code, targetObject.url);
+            assert.equal(test.code, o.code);
+            assert.equal(test.message, targetObject.html);
+        } catch(e) {
+            return console.log(clc.red(JSON.stringify(e)));
+        }
+        console.log(clc.green('All good, ' + o.code));
+    });
+}
+
+tests.forEach(testFn);
