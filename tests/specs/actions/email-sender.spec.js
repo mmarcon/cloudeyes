8ddf493574e3a884243bff6128bf622bbdec5768
Massimiliano Marcon <emaildelmax@gmail.com>
2013-08-01 11:48:30 -0400

    Unit tests for email-sender

--- tests/specs/actions/email-sender.spec.js
+++ tests/specs/actions/email-sender.spec.js
@@ -22,6 +22,8 @@ describe('Email Sender', function(){
     afterEach(function() {
         mockery.disable();
         mockery.deregisterAll();
+        emailServerMock.send.reset();
+        emailjsMock.server.connect.reset();
     });
     it('connects to the mail server', function(){
         mockery.registerMock('emailjs/email', emailjsMock);
@@ -52,6 +54,169 @@ describe('Email Sender', function(){
             user: 'user',
             password: 'password',
             ssl: true
+        });
+    });
+    it('attemps to send one email for each of the recipents', function(){
+        mockery.registerMock('emailjs/email', emailjsMock);
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+        
+        var EmailSender = testutils.requireLocalModule('lib/actions/email-sender');
+        EmailSender({
+            emailserver: {
+                host: 'smtp.gmail.com',
+                user: 'user',
+                password: 'password',
+                ssl: true
+            },
+            plain: 'Hello dude!',
+            html: '<html><body>Hello dude!</body></html>',
+            from: 'hello@cloudey.es',
+            subject: 'Cloudeyes says hello',
+            recipients: [
+                'max@cloudey.es',
+                'cloud@cloudey.es'
+            ]
+        });
+        
+        expect(emailServerMock.send.callCount).toBe(2);
+        expect(emailServerMock.send.mostRecentCall.args[0]).toEqual({
+            text:    'Hello dude!',
+            from:    'hello@cloudey.es',
+            to:      'cloud@cloudey.es',
+            subject: 'Cloudeyes says hello',
+            attachment: [
+                {data: '<html><body>Hello dude!</body></html>', alternative:true}
+            ]
+        });
+    });
+    it('resolves the promise when at least one email is sent successfully', function(){
+        mockery.registerMock('emailjs/email', emailjsMock);
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+        
+        emailServerMock.send.andCallFake(function(options, callback){
+            if(options.to === 'max@cloudey.es') {
+                callback(new Error(':('), null);
+            } else {
+                callback(null, 'Yep!');
+            }
+        });
+        
+        var EmailSender = testutils.requireLocalModule('lib/actions/email-sender');
+        
+        var promise = EmailSender({
+            emailserver: {
+                host: 'smtp.gmail.com',
+                user: 'user',
+                password: 'password',
+                ssl: true
+            },
+            plain: 'Hello dude!',
+            html: '<html><body>Hello dude!</body></html>',
+            from: 'hello@cloudey.es',
+            subject: 'Cloudeyes says hello',
+            recipients: [
+                'max@cloudey.es',
+                'cloud@cloudey.es'
+            ]
+        });
+        
+        promise.then(function(o){
+            expect(o.successfulEmails).toBeTruthy();
+            expect(o.successfulEmails.length).toBe(1);
+            expect(o.failedEmails).toBeTruthy();
+            expect(o.failedEmails.length).toBe(1);
+        });
+        
+        promise.catch(function(o){
+            expect('Should not').toBe('here');
+        });
+    });
+    it('resolves the promise when at all emails are sent successfully', function(){
+        mockery.registerMock('emailjs/email', emailjsMock);
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+        
+        emailServerMock.send.andCallFake(function(options, callback){
+            callback(null, 'Yep!');
+        });
+        
+        var EmailSender = testutils.requireLocalModule('lib/actions/email-sender');
+        
+        var promise = EmailSender({
+            emailserver: {
+                host: 'smtp.gmail.com',
+                user: 'user',
+                password: 'password',
+                ssl: true
+            },
+            plain: 'Hello dude!',
+            html: '<html><body>Hello dude!</body></html>',
+            from: 'hello@cloudey.es',
+            subject: 'Cloudeyes says hello',
+            recipients: [
+                'max@cloudey.es',
+                'cloud@cloudey.es'
+            ]
+        });
+        
+        promise.then(function(o){
+            expect(o.successfulEmails).toBeTruthy();
+            expect(o.successfulEmails.length).toBe(2);
+            expect(o.failedEmails).toBeTruthy();
+            expect(o.failedEmails.length).toBe(0);
+        });
+        
+        promise.catch(function(o){
+            expect('Should not').toBe('here');
+        });
+    });
+    it('rejects the promise when at all emails fail to be sent', function(){
+        mockery.registerMock('emailjs/email', emailjsMock);
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+        
+        emailServerMock.send.andCallFake(function(options, callback){
+            callback(new Error(':('));
+        });
+        
+        var EmailSender = testutils.requireLocalModule('lib/actions/email-sender');
+        
+        var promise = EmailSender({
+            emailserver: {
+                host: 'smtp.gmail.com',
+                user: 'user',
+                password: 'password',
+                ssl: true
+            },
+            plain: 'Hello dude!',
+            html: '<html><body>Hello dude!</body></html>',
+            from: 'hello@cloudey.es',
+            subject: 'Cloudeyes says hello',
+            recipients: [
+                'max@cloudey.es',
+                'cloud@cloudey.es'
+            ]
+        });
+        
+        promise.catch(function(o){
+            expect(o.successfulEmails).toBeTruthy();
+            expect(o.successfulEmails.length).toBe(0);
+            expect(o.failedEmails).toBeTruthy();
+            expect(o.failedEmails.length).toBe(2);
+        });
+        
+        promise.then(function(o){
+            expect('Should not').toBe('here');
         });
     });
 });
