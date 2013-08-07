0e01bfcc701fcaeca419c54a01024b70dd5271ea
Massimiliano Marcon <emaildelmax@gmail.com>
2013-08-07 08:29:07 -0400

    CLI Executor that runs a series of commands in order.
    Uses spawn and promises to guarantee the execution order.
    
    [ci skip]

---
+++ deploy/appfog/deploy.js
@@ -0,0 +1,45 @@
+var setup = require('./setup.json'),
+    CLIExecutor = require('../utils/cli-executor');
+
+var COMMANDS = {
+    main: 'af',
+    env: function(app, variable, value){
+        return [COMMANDS.main, 'env-add', app, variable + '=' + value].join(' ');
+    },
+    update: function(app){
+        return [COMMANDS.main, 'update', app].join(' ');
+    }
+};
+
+var executor = new CLIExecutor();
+
+var key = setup.key;
+
+//Setup the master first
+// var master = setup.master;
+// exec(COMMANDS.env(master.name, 'NODE_TYPE', 'master'), function(error, stdout, stderr){
+//     var std = stdout || stderr;
+//     console.log(std);
+// });
+// exec(COMMANDS.env(master.name, 'KEY', key), function(error, stdout, stderr){
+//     var std = stdout || stderr;
+//     console.log(std);
+// });
+// exec(COMMANDS.env(master.name, 'KEY', key), function(error, stdout, stderr){
+//     var std = stdout || stderr;
+//     console.log(std);
+// });
+// exec(COMMANDS.update(master.name), {cwd: '..'}, function(error, stdout, stderr){
+//     var std = stdout || stderr;
+//     console.log(std);
+// });
+
+//Now the eyes
+var eyes = setup.eyes;
+eyes.forEach(function(eye){
+    executor.exec(COMMANDS.env(eye.name, 'NODE_TYPE', 'eye'));
+    executor.exec(COMMANDS.env(eye.name, 'KEY', key));
+    executor.exec(COMMANDS.update(eye.name), {cwd: '../..'});
+});
+
+executor.start();
---
+++ deploy/utils/cli-executor.js
@@ -0,0 +1,48 @@
+var spawn = require('child_process').spawn,
+    Q = require('q');
+    
+function printout(data){
+    console.log(data.toString('utf8'));
+}
+
+function CLIExecutor(){
+    this.queue = [];
+};
+
+CLIExecutor.prototype.exec = function(command, options){
+    this.queue.push({
+        command: command,
+        options: options
+    });
+};
+
+CLIExecutor.prototype.start = function(ondone, onerror){
+    var tasks = [];
+    this.queue.forEach(function(t){
+        var fn = function(){
+            var deferred = Q.defer();
+            console.log('running ' + t.command);
+            var process = spawn(t.command, [], t.options || {});
+            process.stdout.on('data', printout);
+            process.stderr.on('data', printout);
+            process.on('exit', function(code, signal){
+                if(code === 0) {
+                    return deferred.resolve();
+                }
+                deferred.reject(code);
+            });
+            return deferred;
+        };
+        tasks.push(fn);
+    });
+    Q.all(tasks.map(function(task){
+        return task();
+    })).done(ondone, onerror);
+};
+
+// var ce = new CLIExecutor();
+// ce.exec('ls');
+// ce.exec('ls', {cwd: '..'});
+// ce.start();
+
+module.exports = CLIExecutor;
