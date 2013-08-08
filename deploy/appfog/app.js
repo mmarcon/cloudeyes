4e71e12d4f0d8af66bef80db455f5cdb69ab68b5
Massimiliano Marcon <me@marcon.me>
2013-08-08 21:39:53 +0200

    Invoke MasterExecutor.execute from app.js

--- deploy/appfog/app.js
+++ deploy/appfog/app.js
@@ -1,21 +1,28 @@
-var Eye = require('../../lib/nodes/eye');
+var Eye = require('../../lib/nodes/eye'),
+    MasterExexutor = require('../../lib/nodes/master-executor'),
+    config = require('./master-config.json');
 
 //Get a picture of the environment
-var HOST = process.env.VCAP_APP_HOST,
-    PORT = process.env.VCAP_APP_PORT,
+var PORT = process.env.VCAP_APP_PORT,
     NODE_TYPE = process.env.NODE_TYPE,
     REGION = process.env.REGION,
     KEY = process.env.KEY;
 
 if(NODE_TYPE === 'master') {
     //Then I need to make a master node
-    console.log(HOST);
+    MasterExexutor.execute({
+        eyes: config.eyes,
+        hosts: config.hosts,
+        me: config.endpoint,
+        port: PORT, //Port we listen to is not necessarily the port where we can be reached at
+        key: KEY
+    });
 }
 else {
     //Well, it's an eye
     var Eye = new Eye({
         debug: false,
-        port: PORT,
+        port: PORT, //Port we listen to is not necessarily the port where we can be reached at
         key: KEY,
         region: REGION,
         alwaysUseDefaultPorts: true
